1879d3cfd977d39f021bbb6a1c518bcbfbd5e485
rgliuca <44307228+rgliuca@users.noreply.github.com>
2022-03-11 16:31:40 -0800

    Add files via upload

---
+++ wordle_dictionary.py
@@ -0,0 +1,18 @@
+class WordleDictionary:
+	def __init__(self, dictionary_file, common_word_file):
+		# dictonary_file is the file name for five_letter_words.txt
+		# common_word_file is the file name for five_letter_words_common.txt
+		# in this method, you'll want to read the two text files and save the
+		# words in two separate lists (self._words and self._common_words)
+		pass
+
+	def valid_word(self, word):
+		# checks if word is a valid word against the dictionary (self._words)
+		# returns True/False
+		pass
+
+	def get_random_word(self):
+		# returns a random word from common_word_file
+		# keep track of words used already and skip these words
+		pass
+	
