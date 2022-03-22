35eed0f985a556352bcdebcfc39483f2c370b9e1
rgliuca <44307228+rgliuca@users.noreply.github.com>
2022-03-21 18:27:58 -0700

    Update wordle_board.py

--- wordle_board.py
+++ wordle_board.py
@@ -1,6 +1,6 @@
 class WordleBoard:
 	# The board knows the_word, the board can take a guess from the player, and the board would know when all the lines have been exhausted!
-	def __init__(self, xpos, ypos, the_word):
+	def __init__(self, display, xpos, ypos, the_word):
 		self._the_word = the_word
 		self._wordle_lines = [WordleLine(xpos, ypos + i*….  , the_word, "")   for i in range(6)]
 	
@@ -10,4 +10,8 @@ class WordleBoard:
 		
 	def num_tries_left():
 		# returns the num of empty wordle lines
+		pass
+	
+	def rasterize():
+		# displays the board
 		pass
